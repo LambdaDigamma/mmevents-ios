2855bf7a86ab20f9f869ad02c8ed04600d7e6589
Lennart Fischer <github@lambdadigamma.com>
2021-06-13 22:17:52 +0200

    Updated Dependencies

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "MMAPI", url: "https://github.com/LambdaDigamma/mmapi-ios", from: "0.2.0"),
-        .package(name: "MMUI", url: "https://github.com/LambdaDigamma/mmui-ios", .branch("master")),
-        .package(name: "MMCommon", url: "https://github.com/lambdadigamma/mmcommon-ios", .branch("master")),
-        .package(name: "MMPages", url: "https://github.com/lambdadigamma/mmpages-ios", .branch("main")),
+        .package(name: "MMUI", url: "https://github.com/LambdaDigamma/mmui-ios", from: "0.2.1"),
+        .package(name: "MMCommon", url: "https://github.com/lambdadigamma/mmcommon-ios", from: "0.0.1"),
+        .package(name: "MMPages", url: "https://github.com/lambdadigamma/mmpages-ios", from: "0.0.2"),
         .package(name: "ModernNetworking", url: "https://github.com/lambdadigamma/modernnetworking", from: "0.1.1"),
         .package(name: "Gestalt", url: "https://github.com/regexident/Gestalt", from: "2.1.0"),
         .package(url: "https://github.com/DeclarativeHub/Bond.git", from: "7.8.0"),
         .package(url: "https://github.com/DeclarativeHub/ReactiveKit.git", from: "3.18.0"),
-//        .package(name: "MMPlaces", url: "https://github.com/lambdadigamma/mmpages-ios", .branch("main")),
+//        .package(name: "MMPlaces", url: "https://github.com/lambdadigamma/mmplaces-ios", .branch("main")),
     ],
     targets: [
         .target(
